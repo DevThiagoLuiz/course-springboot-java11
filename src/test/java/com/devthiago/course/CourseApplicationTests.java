e64a0840c513871dcb4d238617889a3b9082a9ad
DevThiagoLuiz <thiagoluiz5123@gmail.com>
2022-05-26 00:37:06 -0300

    Project created

---
+++ src/test/java/com/devthiago/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthiago.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

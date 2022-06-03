342bd439bee4941ef1a397388dbc0351bc7f1d03
DevThiagoLuiz <thiagoluiz5123@gmail.com>
2022-06-03 00:27:46 -0300

    Exception handling - delete

--- src/main/java/com/devthiago/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devthiago/course/resources/exceptions/ResourceExceptionHandler.java
@@ -13,6 +13,8 @@ import com.devthiago.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
@@ -21,4 +23,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	} 
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	} 
+	
 }
